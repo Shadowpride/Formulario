67f0554b59298c99e569267307454279dc3c4867
Shadowpride <60741137+Shadowpride@users.noreply.github.com>
2020-11-03 10:55:19 -0300

    1.0

--- app/src/main/java/uy/shadow/formulario/MainActivity.java
+++ app/src/main/java/uy/shadow/formulario/MainActivity.java
@@ -1,12 +1,18 @@
 package uy.shadow.formulario;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         final Button btnEditar = findViewById(R.id.ButtonR);
 
         final EditText Nom = findViewById(R.id.NombreR);
-        final EditText Fech = findViewById(R.id.FechaR);
+        final View Fech = findViewById(R.id.FechaR);
         final EditText Tel = findViewById(R.id.TelefonoR);
         final EditText Eml = findViewById(R.id.EmailR);
         final EditText Desc = findViewById(R.id.DescripcionR);
@@ -27,19 +33,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                // Creo el intent para ir a la otra activity
                 Intent enviarDatos = new Intent(MainActivity.this, DetallesFormulario.class);
-
-                //Agrego los datos al intent
                 enviarDatos.putExtra("Nombre", Nom.getText().toString());
-                enviarDatos.putExtra("Fecha", Fech.getText().toString());
+                enviarDatos.putExtra("Fecha", Fech.toString());
                 enviarDatos.putExtra("Telefono", Tel.getText().toString());
                 enviarDatos.putExtra("Email", Eml.getText().toString());
                 enviarDatos.putExtra("Descripcion", Desc.getText().toString());
-
-                //Inicio la activity
                 startActivity(enviarDatos);
+                finish();
             }
         });
     }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
